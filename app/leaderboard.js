6ac37ef5ca88e1adab9df684b9a5ae6f077eabba
chadmorita <School@Chads-MacBook-Pro-4.local>
2016-10-12 16:30:34 -1000

    Databases, Part 2 and Forms

--- app/leaderboard.js
+++ app/leaderboard.js
@@ -1,11 +1,10 @@
 PlayersList = new Mongo.Collection('players');
 
 if(Meteor.isClient) {
-  console.log("Hello World");
 
   Template.leaderboard.helpers({
     'player': function(){
-      return PlayersList.find();
+      return PlayersList.find({}, { sort: {score: -1, name: 1} });
     },
     'selectedClass': function() {
       var playerId = this._id;
@@ -14,6 +13,10 @@ if(Meteor.isClient) {
         return "selected";
       }
     },
+    'selectedPlayer': function () {
+      var selectedPlayer = Session.get('selectedPlayer');
+      return PlayersList.findOne({ _id: selectedPlayer});
+    }
   });
 
   Template.leaderboard.events({
@@ -21,9 +24,32 @@ if(Meteor.isClient) {
       var playerId = this._id;
       Session.set('selectedPlayer', playerId);
       var selectedPlayer = Session.get('selectedPlayer');
-      console.log(selectedPlayer);
+    },
+    'click .increment': function () {
+      var selectedPlayer = Session.get('selectedPlayer');
+      PlayersList.update({ _id: selectedPlayer }, { $inc: {score: 5}});
+    },
+    'click .decrement': function () {
+      var selectedPlayer = Session.get('selectedPlayer');
+      PlayersList.update({ _id: selectedPlayer }, { $inc: {score: -5}});
+    },
+    'click .remove': function () {
+      var selectedPlayer = Session.get('selectedPlayer');
+      PlayersList.remove({ _id: selectedPlayer });
     }
-  })
+  });
+
+  Template.addPlayerForm.events({
+    'submit form': function (event) {
+      event.preventDefault();
+      var playerNameVar = event.target.playerName.value;
+      PlayersList.insert({
+        name: playerNameVar,
+        score: 0
+      })
+      event.target.playerName.value = "";
+    }
+  });
 }
 
 if(Meteor.isServer){
